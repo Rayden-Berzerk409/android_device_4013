b11a1119aa0e9d222b8227618b76698ed40426ae
Rayden_Berzerk409 <juandavidbogallo2020@gmail.com>
2024-03-14 18:08:31 -0500

    AndroidProducts.mk: add common lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,8 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/device_4013.mk
+    $(LOCAL_DIR)/device_4013.mk \
     $(LOCAL_DIR)/rr_4013.mk
+
+COMMON_LUNCH_CHOICES := \
+    rr_4013-user \
+    rr_4013-userdebug \
+    rr_4013-eng 
